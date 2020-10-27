6644ac8ef8bed43bdcd4a0a8104538119d06fde7
Alexander Jabbour <ajabbour@uwaterloo.ca>
2020-10-27 14:09:49 -0400

    making this whole thing a go script instead

---
+++ PredictPi.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+	// initialize variables for number of coordinates within/outside the quarter circle
+	var inside int = 0
+	var outside int = 0
+	for i := 0; i < 1000000; i++ {
+		// generate the random coordinates
+		x = rand.Intn(1000000)/1000000
+		y = rand.Intn(1000000)/1000000
+
+		// if the coordinate is within the quarter circle
+	}
+}

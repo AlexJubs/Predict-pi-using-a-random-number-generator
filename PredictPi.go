88e7f4737b78506bb4a9005cc4aaa15de7ed3a00
Alexander Jabbour <ajabbour@uwaterloo.ca>
2020-10-27 14:17:07 -0400

    printing coordinates

--- PredictPi.go
+++ PredictPi.go
@@ -9,11 +9,20 @@ func main() {
 	// initialize variables for number of coordinates within/outside the quarter circle
 	var inside int = 0
 	var outside int = 0
+	var x float64
+	var y float64
 	for i := 0; i < 1000000; i++ {
 		// generate the random coordinates
-		x = rand.Intn(1000000)/1000000
-		y = rand.Intn(1000000)/1000000
+		x = rand.Float64()
+		y = rand.Float64()
 
 		// if the coordinate is within the quarter circle
+		fmt.Print(x, y)
+    	fmt.Println()
+
+    	if (x*x + y*y <= 1) {
+    		inside++
+    	}
+    	outside++
 	}
 }
